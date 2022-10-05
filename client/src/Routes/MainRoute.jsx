c8df03f87a4b5a1f26fdab738e06d862b14646b1
Saurabh <singhsaurabh162000@gmail.com>
2022-10-05 22:58:04 +0530

    uploading utill folder

---
+++ client/src/Routes/MainRoute.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Calender from "../components/calender/Calender";
+import CreateNewProject from "../components/calender/CreateNewProject";
+import { Routes, Route } from "react-router-dom";
+import EditProject from "../components/calender/EditProject";
+import Homepage from "../components/HomePage/Homepage";
+import { Register } from "../components/register/Register";
+import { Login } from "../components/login/Login";
+const MainRoute = () => {
+  return (
+    <Routes>
+      <Route path="/homepage" element={<Homepage />} />
+      <Route path="/calender" element={<Calender />} />
+      <Route path="newproject" element={<CreateNewProject />} />
+      <Route path="editproject" element={<EditProject />} />
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/register" element={<Register />}></Route>
+    </Routes>
+  );
+};
+
+export default MainRoute;
