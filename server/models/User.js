8607b9ecd48dc5041b04b115013021d6f828e6f4
Saurabh <singhsaurabh162000@gmail.com>
2022-10-08 16:58:36 +0530

    updating user schema and tasks

--- server/models/User.js
+++ server/models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     profilePicture: { type: String, default: '' },
